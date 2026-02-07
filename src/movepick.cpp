56d9b515fbcdbecaf82d74ec39b9290676dde99e
Антон Стецко <64554404+Anroshka@users.noreply.github.com>
2026-02-07 05:53:05 +0300

    Enhance move scoring in MovePicker by refining capture and history value calculations

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -182,9 +182,15 @@ ExtMove* MovePicker::score(MoveList<Type>& ml) {
         else  // Type == EVASIONS
         {
             if (pos.capture_stage(m))
-                m.value = PieceValue[capturedPiece] + (1 << 28);
+                m.value = (*captureHistory)[pc][to][type_of(capturedPiece)]
+                        + 7 * int(PieceValue[capturedPiece])
+                        + (1 << 28);
             else
-                m.value = (*mainHistory)[us][m.raw()] + (*continuationHistory[0])[pc][to];
+                m.value = 2 * (*mainHistory)[us][m.raw()]
+                        + 2 * sharedHistory->pawn_entry(pos)[pc][to]
+                        + (*continuationHistory[0])[pc][to]
+                        + (*continuationHistory[1])[pc][to]
+                        + (*continuationHistory[3])[pc][to];
         }
     }
     return it;
